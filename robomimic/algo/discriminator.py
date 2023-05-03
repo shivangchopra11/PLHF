9e30251e3e6c6e42d19d852689d328a48c12fd46
Shivang Chopra <shivang@Shivangs-MacBook-Pro-2.local>
2023-05-04 02:03:08 +0530

    New changes

--- robomimic/algo/discriminator.py
+++ robomimic/algo/discriminator.py
@@ -1,4 +1,13 @@
+## Refer to https://github.com/MaxDu17/BehaviorRetrieval/blob/8088fe9bb6b49f2147b649a440a899624901d789/robomimic/docs/modules/algorithms.md
+
 from robomimic.algo import register_algo_factory_func, WeighingAlgo
+
+@register_algo_factory_func("rnn_discriminator")
+def algo_config_to_class(algo_config = None):
+    """
+    Yields the class for the weighing algorithm. Can be expanded to accomodate more fancier classifiers
+    """
+    return TemporalEmbeddingWeighter, {}
 
 class RNNDiscriminator(WeighingAlgo):
     def _create_networks(self):
---
+++ robomimic/config/discriminator_config.py
@@ -0,0 +1,9 @@
+## Refer to https://github.com/MaxDu17/BehaviorRetrieval/blob/8088fe9bb6b49f2147b649a440a899624901d789/robomimic/docs/modules/configs.md
+from robomimic.config.base_config import BaseConfig
+
+class RNNDiscriminatorConfig(BaseConfig):
+    def train_config(self):
+        pass
+    def algo_config(self):
+        pass
+
